5478d2cdf06b1d29451d195cce78a3e971a7263c
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-07 18:22:53 -0600

    Fix whitespace issue in category index.blade.php

--- resources/views/almacen/categoria/index.blade.php
+++ resources/views/almacen/categoria/index.blade.php
@@ -65,7 +65,7 @@
                                 <tr>
                                     <td>
                                         <!-- Action buttons -->
-                                        <a href="{{ route('categoria.edit', $cat->id_categoria)}}" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
+                                        <a href="{{ route('categoria.edit', $cat->id_categoria) }}" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
                                         <!-- Button trigger for danger theme modal -->
                                         <button type="button" class="btn btn-outline-danger btn-sm" data-toggle="modal" data-target="#modal-delete-{{ $cat->id_categoria }}"><i class="fas fa-trash-alt"></i></button>
                                     </td>
