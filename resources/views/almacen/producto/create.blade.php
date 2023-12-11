7562c0486ac8c21a440957a20363e329573d36b5
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-11 11:07:14 -0600

    Refactor create.blade.php for new product form

--- resources/views/almacen/producto/create.blade.php
+++ resources/views/almacen/producto/create.blade.php
@@ -5,34 +5,85 @@
         <!-- general form elements -->
         <div class="card card-primary">
             <div class="card-header">
-            <h3 class="card-title">Nueva Categoria</h3>
-        </div>
-        <!-- /.card-header -->
+                <h3 class="card-title">Nuevo Producto</h3>
+            </div>
+            <!-- /.card-header -->
 
-        <!-- form start -->
-        <form action="{{ route('categoria.store') }}" method="POST" class="form">
-            @csrf
-            <div class="card-body">
-                <div class="form-group">
-                    <label for="categoria">Nombre</label>
-                    <input type="text" class="form-control" name="categoria" id="categoria"
-                        placeholder="Ingrese el nombre de la categoria">
-                </div>
+            <!-- form start -->
+            <form action="{{ route('producto.store') }}" method="POST" class="form">
+                @csrf
+                <div class="card-body">
                     <div class="form-group">
-                        <label for="descripcion">Descripcion</label>
-                        <input type="text" class="form-control" name="descripcion" id="descripcion"
-                            placeholder="Ingrese la descripcion de la categoria">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" id="nombre"
+                            placeholder="Ingrese el nombre del nuevo producto">
                     </div>
-            </div>
-            <!-- /.card-body -->
 
-            <div class="card-footer">
-                <button type="submit" class="btn btn-success me-1 mb-1">Guardar</button>
-                <button type="reset" class="btn btn-danger me-1 mb-1">Cancelar</button>
-            </div>
-        </form>
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label>Categoria</label>
+                            <select name="id_categoria" class="form-control" id="idcategoria">
+                                @foreach ($categorias as $cat)
+                                    <option value="{{ $cat->categoria }}">{{ $cat->categoria }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="codigo">Codigo</label>
+                            <input type="text" class="form-control" name="codigo" id="codigo"
+                                placeholder="Ingrese el codigo del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="stock">Stock</label>
+                            <input type="text" class="form-control" name="stock" id="stock"
+                                placeholder="Ingrese el stock del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="unidad">Unidad</label>
+                            <select name="unidad" class="form-control" id="unidad">
+                                <option>Pieza</option>
+                                <option>Kilos</option>
+                                <option>Cajas</option>
+                                <option>Paquetes</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="descripcion">Descripcion</label>
+                            <input type="text" class="form-control" name="descripcion" id="descripcion"
+                                placeholder="Ingrese la descripcion del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="imagen">Imagen</label>
+                            <input type="file" class="form-control" id="imagen" name="imagen">
+                        </div>
+                    </div>
+
+
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-success me-1 mb-1">Guardar</button>
+                        <button type="reset" class="btn btn-danger me-1 mb-1">Cancelar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- /.card -->
     </div>
-    <!-- /.card -->
-</div>
-<!-- /.row -->
+    <!--/.row -->
 @endsection
