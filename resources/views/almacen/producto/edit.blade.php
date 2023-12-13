751e1c2421709a24c8a809adf3fd30338f20ee38
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-13 11:10:18 -0600

    Refactor edit.blade.php to update product form layout

--- resources/views/almacen/producto/edit.blade.php
+++ resources/views/almacen/producto/edit.blade.php
@@ -1,39 +1,85 @@
 @extends('layouts.admin')
 @section('contenido')
     <!-- left colum -->
-    <div class="col-md-6">
+    <div class="col-md-12">
         <!-- general form elements -->
         <div class="card card-primary">
             <div class="card-header">
-            <h3 class="card-title">Editar Categoria {{ $categoria->categoria}}</h3>
+                <h3 class="card-title">Editar Producto</h3>
+            </div>
+            <!-- /.card-header -->
+
+            <!-- form start -->
+            <form action="{{ route('producto.update', $producto->id_producto) }}" method="POST" enctype="multipart/form-data" class="form">
+                @csrf
+                @method('PUT')
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 col-12">
+                            <div class="form-group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" class="form-control" name="nombre" value="{{ $producto->nombre }}"
+                                    placeholder="Ingrese el nombre del nuevo producto">
+                            </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label>Categoria</label>
+                            <select name="id_categoria" class="form-control" id="id_categoria">
+                                @foreach ($categorias as $cat)
+                                @if ($cat->id_categoria == $producto->id_categoria) //si la categoria es igual a la categoria del producto
+                                    <option value="{{ $cat->id_categoria }}">{{ $cat->categoria }}</option>
+                                    @else //si no es igual
+                                    <option value="{{ $cat->id_categoria }}">{{ $cat->categoria }}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="codigo">Codigo</label>
+                            <input type="text" class="form-control" name="codigo" value="{{ $producto->codigo }}"
+                                placeholder="Ingrese el codigo del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="stock">Stock</label>
+                            <input type="text" class="form-control" name="stock" value="{{ $producto->stock }}"
+                                placeholder="Ingrese el stock del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-12">
+                        <div class="form-group">
+                            <label for="descripcion">Descripcion</label>
+                            <input type="text" class="form-control" name="descripcion" value="{{ $producto->descripcion }}"
+                                placeholder="Ingrese la descripcion del producto">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-12">
+                        <div class="form-group">
+                            <label for="descripcion">Imagen</label>
+                            <input type="file" class="form-control" id="imagen" name="imagen">
+                            @if (($producto->imagen)!="") //si el producto tiene una imagen
+                                <img src="{{ asset('imagenes/productos/' .$producto->imagen) }}" height="100px" width="100px">
+                            @endif
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-success me-1 mb-1">Guardar</button>
+                        <button type="reset" class="btn btn-danger me-1 mb-1">Cancelar</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <!-- /.card-header -->
-
-        <!-- form start -->
-        <form action="{{ route('categoria.update' , $categoria->id_categoria) }}" method="POST" class="form">
-            @csrf
-            @method('PUT')
-            <div class="card-body">
-                <div class="form-group">
-                    <label for="categoria">Nombre</label>
-                    <input type="text" class="form-control" name="categoria" id="categoria" value="{{ $categoria->categoria }}"
-                        placeholder="Ingrese el nombre de la categoria">
-                </div>
-                <div class="form-group">
-                    <label for="descripcion">Descripcion</label>
-                    <input type="text" class="form-control" name="descripcion" id="descripcion" value="{{ $categoria->descripcion }}"
-                        placeholder="Ingrese la descripcion de la categoria">
-                </div>
-            </div>
-            <!-- /.card-body -->
-
-            <div class="card-footer">
-                <button type="submit" class="btn btn-success me-1 mb-1">Guardar</button>
-                <button type="reset" class="btn btn-danger me-1 mb-1">Cancelar</button>
-            </div>
-        </form>
+        <!-- /.card -->
     </div>
-    <!-- /.card -->
-    </div>
-    <!-- /.row -->
+    <!--/.row -->
 @endsection
