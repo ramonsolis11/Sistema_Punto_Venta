421f0f37494f4ecb9e378f9917a7d97020fa1d75
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-13 11:55:01 -0600

    Update delete modal for product

--- resources/views/almacen/producto/modal.blade.php
+++ resources/views/almacen/producto/modal.blade.php
@@ -1,23 +1,27 @@
-<div class="modal fade" id="modal-delete-{{ $cat->id_categoria }}">
+<div class="modal fade" id="modal-delete-{{ $prod->id_producto }}">
     <div class="modal-dialog">
-        <form action="{{ route('categoria.destroy', $cat->id_categoria) }}" method="POST">
+        <form action="{{ route('producto.destroy', $prod->id_producto) }}" method="POST"> // Ruta para eliminar
             @csrf
-            @method('DELETE')
+            @method('DELETE') // Método para eliminar
             <div class="modal-content bg-danger">
                 <div class="modal-header">
-                    <h4 class="modal-title">Eliminar Categoría</h4>
+                    <h4 class="modal-title">Eliminar Producto</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="close">
                         <span aria-hidden="true">&times;</span>
                 </button>
                 </div>
                 <div class="modal-body">
-                    <p>¿Estás seguro de eliminar la categoría {{ $cat->categoria }}</p>
+                    <p>¿Estás seguro de eliminar el producto {{ $prod->nombre }}</p> // Mensaje de confirmación
                 </div>
                 <div class="modal-footer justify-content-between">
                     <button type="button" class="btn btn-outline-light" data-dismiss="modal">cerrar</button>
                     <button type="submit" class="btn btn-outline-light">Eliminar</button>
                 </div>
             </div>
+            <!-- /.modal-content -->
         </form>
     </div>
+    <!-- /.modal-dialog -->
 </div>
+<!-- /.modal -->
+
