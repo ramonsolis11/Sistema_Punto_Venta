1671abde109e2580c55932021b4b56be664d451c
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-14 21:48:39 -0600

    Add modal.blade.php

---
+++ resources/views/ventas/clientes/modal.blade.php
@@ -0,0 +1,23 @@
+<div class="modal fade" id="modal-delete-{{ $cli->id_persona }}">
+    <div class="modal-dialog">
+        <form action="{{ route('cliente.destroy', $cli->persona) }}" method="POST">
+            @csrf 
+            @method('DELETE') <!-- para que el formulario sepa que se va a eliminar -->
+            <div class="modal-content bg-danger">
+                <div class="modal-header">
+                    <h4 class="modal-title">Eliminar Cliente</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>¿Estás seguro de eliminar el cliente {{ $cli->nombre }}</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-outline-light" data-dismiss="modal">cerrar</button>
+                    <button type="submit" class="btn btn-outline-light">Eliminar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
