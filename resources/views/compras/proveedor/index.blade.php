1a4ad8c7e49d6b061b91872e9e2603c740e7b68f
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-28 12:13:52 -0600

    Update user authentication logic

---
+++ resources/views/compras/proveedor/index.blade.php
@@ -0,0 +1,94 @@
+@extends ('layouts.admin')
+@section ('contenido')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">LISTADO DE PROVEEDORES</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+                    <li class="breadcrumb-item active">Proveedores</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Hoverable rows start -->
+<section class="section">
+    <div class="row" id="table-hover-row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="col-xl-12">
+                        <form action="{{ route('proveedor.index') }}" method="get">
+
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="input-group mb-6">
+                                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+                                        <input type="text" class="form-control" name="texto" placeholder="Buscar proveedor" value="{{$texto}}" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Buscar</button>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="input-group mb-6">
+                                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-plus-circle-fill"></i></span>
+                                        <a href="{{ route('proveedor.create') }}" class="btn btn-success">Nuevo</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                    </div>
+                    <!-- table hover -->
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-0">
+                            <thead>
+                                <tr>
+                                    <th>Opciones</th>
+                                    <th>Nombre</th>
+                                    <th>Tipo Documento</th>
+                                    <th>Número Documento</th>
+                                    <th>Dirección</th>
+                                    <th>Teléfono</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($proveedor as $prov)
+                                <tr>
+                                    <td>
+                                        <!-- Action buttons -->
+                                        <a href="{{route('proveedor.edit' , $prov->id_persona)}}" class="btn btn-warning btn-sm"><i class="fas fa-pen"></i></a>
+                                        <!-- Button trigger for danger theme modal -->
+                                        <button type="button" class="btn btn-outline-danger btn-sm" data-toggle="modal" data-target="#modal-delete-{{ $prov->id_persona }}"><i class="fas fa-trash-alt"></i></button>
+                                    </td>
+                                    <td>{{ $prov->nombre}}</td>
+                                    <td>{{ $prov->tipo_documento}}</td>
+                                    <td>{{ $prov->num_documento}}</td>
+                                    <td>{{ $prov->direccion}}</td>
+                                    <td>{{ $prov->telefono}}</td>
+                                    <td>{{ $prov->email}}</td>
+
+                                </tr>
+                                @include('compras.proveedor.modal')
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $proveedor->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
