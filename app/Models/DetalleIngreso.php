98bdb449fff8a8ab0ac5f45a969d8e84686b5db4
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-12-28 18:39:10 -0600

    Refactor DetalleIngreso model to Categoria model

--- app/Models/DetalleIngreso.php
+++ app/Models/DetalleIngreso.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DetalleIngreso extends Model
+class Categoria extends Model
 {
     use HasFactory;
+
+    protected $table = 'detalle_ingreso';
+
+    protected $primaryKey = 'id_detalle_ingreso';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_ingreso',
+        'id_producto',
+        'cantidad',
+        'precio_compra',
+        'precio_venta'
+    ];
+
+    protected $guarded = [
+
+    ];
 }
